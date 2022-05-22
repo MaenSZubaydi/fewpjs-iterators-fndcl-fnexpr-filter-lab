2e7c91bccc7c9f4660fd2f6050b943ca09bf7c16
MaenSZubaydi <maenzubaydi@gmail.com>
2022-05-22 10:15:59 +1000

    commit tree bc9705890ae732ae280b0e94ea3fadcd83784d03

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(array, value){
+    let arrayNew = [];
+    for (let i = 0; i<array.length;i++){
+        if (array[i]===value){
+            arrayNew.push(value);
+            console.log(arrayNew);
+        }
+    }
+    console.log(arrayNew);
+    return arrayNew;
+}
